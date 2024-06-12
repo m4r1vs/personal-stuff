0286de726cef389760faa5aa4cfdbc0b6ca494e5
Marius Niveri <mniveri@cc.systems>
2024-06-12 09:40:46 +0200

    overwrite previous keyboard toggl notificaiton

--- toggle-keymap.sh
+++ toggle-keymap.sh
@@ -2,10 +2,10 @@
 
 if [ "$(cat /home/mn/.keymap-state)" == "de" ]; then
   echo "us" > ~/.keymap-state
-  notify-send "US keyboard layout" -t 1100 -i /usr/share/icons/Papirus/48x48/categories/preferences-desktop-keyboard-shortcuts.svg
+  notify-send "US keyboard layout" -h string:x-dunst-stack-tag:tmux-session-switch -t 1100 -i /usr/share/icons/Papirus/48x48/categories/preferences-desktop-keyboard-shortcuts.svg
   setxkbmap -layout us
 else
   echo "de" > ~/.keymap-state
-  notify-send "German keyboard layout" -t 1100 -i /usr/share/icons/Papirus/48x48/categories/preferences-desktop-keyboard-shortcuts.svg
+  notify-send "German keyboard layout" -h string:x-dunst-stack-tag:tmux-session-switch -t 1100 -i /usr/share/icons/Papirus/48x48/categories/preferences-desktop-keyboard-shortcuts.svg
   setxkbmap -layout de
 fi
